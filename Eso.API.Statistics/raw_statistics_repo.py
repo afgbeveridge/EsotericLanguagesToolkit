9a1c29d6fe9bfe11a3766a439860c0eefcf1cdf5
Tony Beveridge <tony.beveridge@outlook.co.nz>
2021-04-17 14:24:32 +1200

    Basic sanic API surface and partial repo implementation

--- Eso.API.Statistics/raw_statistics_repo.py
+++ Eso.API.Statistics/raw_statistics_repo.py
@@ -3,9 +3,10 @@ import os
 import datetime
 import json
 import glob
-from pathlib import Path
+import pandas as pd
 import pymongo
 from pymongo import MongoClient
+from pandas import json_normalize
 
 def _date_converter(obj):
     if isinstance(obj, datetime.datetime):
@@ -24,10 +25,45 @@ class RawStatisticsRepository():
         pass
 
     def add(self, doc):
-        self._rawCollection().insert_one(doc)
+        self._raw_collection().insert_one(doc)
 
-    def statisticsFor(self, language):
-        pass
-    
-    def _rawCollection(self):
+    def statistics_summary(self, language = None):
+        return self._raw_statistics_head() if language is None else self._raw_statistics_for_language(language)
+
+    def _raw_statistics_head(self):
+        src, df = self._get_raw_statistics()
+        print('Dump json')
+        print(df.head())
+        return df.head().to_json()
+
+    def _raw_statistics_for_language(self, language):
+        src, df = self._get_raw_statistics(language)
+        scs = self._get_single_column_stats(['source', 'Code', 'Length'], df)
+        exeTime = self._get_single_column_stats(['execution', 'Time', 'Milliseconds' ], df)
+        basis = { "name": language, "count": len(df.index) }
+        return src, {**basis, **scs, **exeTime}
+
+    def _get_raw_statistics(self, language = None):
+        coll = self._raw_collection()
+        print('Query')
+        results = coll.find({}, {'_id': False}) if language is None else coll.find({"name": language}, {'_id': False})
+        src = list(results)
+        print('Raw source')
+        print(src)
+        df = json_normalize(src)
+        print('Normalized')
+        print(df)
+        return src, df
+
+    def _get_single_column_stats(self, tags, df):
+        d = {}
+        queryable_title_tag = ''.join([tag for tag in tags])
+        readable_title_tag = ''.join([tag.title() for tag in tags])
+        d['mean{}'.format(readable_title_tag)] = df[queryable_title_tag].mean()
+        d['min{}'.format(readable_title_tag)] = df[queryable_title_tag].min()
+        d['max{}'.format(readable_title_tag)] = df[queryable_title_tag].max()
+        print(d)
+        return d
+
+    def _raw_collection(self):
         return self._client[RawStatisticsRepository.DB][RawStatisticsRepository.RAW_COLLECTION]
--- Eso.API.Statistics/stats.py
+++ Eso.API.Statistics/stats.py
@@ -1,23 +1,25 @@
 
 from sanic import Sanic
 from sanic.response import json
+from sanic.response import raw
+from raw_statistics_repo import RawStatisticsRepository
+from functools import partial
 
 app = Sanic("Eso statistics")
 
 # languages known to have stats, with links
 @app.route('/stats')
-async def test(request):
-    return json({'stats': null})
-
-# languages known to have stats, with links
-@app.route('/stats')
-async def test(request):
-    return json({'stats': null})
+async def top_level_summary(request):
+    repo = RawStatisticsRepository()
+    return raw(repo.statistics_summary(), content_type = 'application/json')
 
 # language stats
 @app.route('/stats/<language>')
-async def test(request, language):
-    return json({'stats': null})
+async def language_stats(request, language):
+    repo = RawStatisticsRepository()
+    only_raw = request.args.get("style") == 'raw'
+    src, summary = repo.statistics_summary(language)
+    return json(src) if only_raw else json(summary)
 
 if __name__ == '__main__':
     app.run()
